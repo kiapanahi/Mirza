ec06d99559acabe36d906b70fa6a834550e762f0
Kia Panahi Rad <kia.panahirad@gmail.com>
2020-03-31 02:40:13 +0430

    Add scaffolded Api project

---
+++ src/Mirza.Web/Controllers/InfoController.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mirza.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InfoController : ControllerBase
+    {
+        [HttpGet("version")]
+        public IActionResult Version()
+        {
+            var ver = Assembly.GetAssembly(typeof(InfoController))
+                .GetName()?.Version?.ToString() ?? "unknown version";
+            return Ok(ver);
+        }
+    }
+}
