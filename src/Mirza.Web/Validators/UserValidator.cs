0a17a4a3831f6454df8473788463874f16121091
Kia Panahi Rad <kia.panahirad@gmail.com>
2020-03-31 03:48:57 +0430

    Add user validator

---
+++ src/Mirza.Web/Validators/UserValidator.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using FluentValidation;
+using Mirza.Web.Models;
+
+namespace Mirza.Web.Validators
+{
+    [SuppressMessage("Naming", "CA1710:Identifiers should have correct suffix", Justification = "<Pending>")]
+    public class UserValidator : AbstractValidator<User>
+    {
+        public UserValidator()
+        {
+            RuleFor(p => p.FirstName)
+                .NotEmpty().WithMessage("FirstName must be a non-empty value")
+                .MaximumLength(40).WithMessage("FirstName must be at most 40 characters long");
+
+            RuleFor(p => p.LastName)
+                .NotEmpty().WithMessage("LastName must be a non-empty value")
+                .MaximumLength(50).WithMessage("LastName must be at most 50 characters long");
+
+            RuleFor(p => p.Email)
+                .NotEmpty().WithMessage("Email must be a non-empty value")
+                .EmailAddress().WithMessage("Email field must abide by the simple email structure. i.e. NAME@DOMAIN.TLD");
+        }
+    }
+}
