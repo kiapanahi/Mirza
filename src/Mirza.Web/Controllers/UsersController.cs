4eb127f3166fea0d714e51146d8be83f3ffcef58
Kia Panahi Rad <kia.panahirad@gmail.com>
2020-03-31 20:29:16 +0430

    Add users controller skeleton

---
+++ src/Mirza.Web/Controllers/UsersController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Mirza.Web.Models;
+
+namespace Mirza.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+
+        // GET: api/Users
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUser()
+        {
+            return await Task.FromResult(StatusCode(501, "Not Implemented")).ConfigureAwait(false);
+        }
+
+        // GET: api/Users/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUser(int id)
+        {
+            return await Task.FromResult(StatusCode(501, "Not Implemented")).ConfigureAwait(false);
+        }
+
+        // PUT: api/Users/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUser(int id, User user)
+        {
+            return await Task.FromResult(StatusCode(501, "Not Implemented")).ConfigureAwait(false);
+        }
+
+        // POST: api/Users
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPost]
+        public async Task<ActionResult<User>> PostUser(User user)
+        {
+            return await Task.FromResult(StatusCode(501, "Not Implemented")).ConfigureAwait(false);
+        }
+
+        // DELETE: api/Users/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<User>> DeleteUser(int id)
+        {
+            return await Task.FromResult(StatusCode(501, "Not Implemented")).ConfigureAwait(false);
+        }
+    }
+}
