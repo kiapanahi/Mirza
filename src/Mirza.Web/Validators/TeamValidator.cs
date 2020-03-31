637f5a8fb866088ac1bcc1c522051a9b0364d80a
Kia Panahi Rad <kia.panahirad@gmail.com>
2020-03-31 13:00:29 +0430

    Add team validator rules and tests

---
+++ src/Mirza.Web/Validators/TeamValidator.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using FluentValidation;
+using Mirza.Web.Models;
+
+namespace Mirza.Web.Validators
+{
+    [SuppressMessage("Naming", "CA1710:Identifiers should have correct suffix", Justification = "<Pending>")]
+    public class TeamValidator : AbstractValidator<Team>
+    {
+        public TeamValidator()
+        {
+            RuleFor(t => t.Name)
+                .NotEmpty().WithMessage("Name must be a non-empty value")
+                .MaximumLength(50).WithMessage("Name must be at most 50 characters");
+        }
+    }
+}
---
+++ tests/Mirza.Web.UnitTests/ModelTests/TeamTests.cs
@@ -0,0 +1,62 @@
+﻿using FluentValidation.TestHelper;
+using Mirza.Web.Models;
+using Mirza.Web.Validators;
+using Xunit;
+
+namespace Mirza.Web.UnitTests.ModelTests
+{
+    public class TeamTests
+    {
+        private readonly TeamValidator _teamValidator;
+        public TeamTests()
+        {
+            _teamValidator = new TeamValidator();
+        }
+
+        [Fact]
+        public void Name_Should_Have_Validation_Error_When_Null()
+        {
+            var t = new Team { Name = null };
+            _teamValidator.TestValidate(t)
+                .ShouldHaveValidationErrorFor(t => t.Name)
+                .WithErrorMessage("Name must be a non-empty value")
+                .WithSeverity(FluentValidation.Severity.Error);
+        }
+
+        [Fact]
+        public void Name_Should_Have_Validation_Error_When_EmptyString()
+        {
+            var t = new Team { Name = string.Empty };
+            _teamValidator.TestValidate(t)
+                .ShouldHaveValidationErrorFor(t => t.Name)
+                .WithErrorMessage("Name must be a non-empty value")
+                .WithSeverity(FluentValidation.Severity.Error);
+        }
+
+        [Fact]
+        public void Name_Should_Have_Validation_Error_When_MoreThan50Characters()
+        {
+            var t = new Team { Name = new string('a', 55) };
+            _teamValidator.TestValidate(t)
+                .ShouldHaveValidationErrorFor(t => t.Name)
+                .WithErrorMessage("Name must be at most 50 characters")
+                .WithSeverity(FluentValidation.Severity.Error);
+        }
+
+        [Fact]
+        public void Name_Should_Be_Valid()
+        {
+            var t = new Team { Name = "sample_name" };
+            _teamValidator.TestValidate(t)
+                .ShouldNotHaveValidationErrorFor(t => t.Name);
+        }
+
+        [Fact]
+        public void Model_Should_Be_Valid()
+        {
+            var t = new Team { Name = "sample_name" };
+            _teamValidator.TestValidate(t)
+                .ShouldNotHaveAnyValidationErrors();
+        }
+    }
+}
