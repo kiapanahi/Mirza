01a49db8956c3b42d8e3d28962c66022de1d3973
Kia Panahi Rad <kia.panahirad@gmail.com>
2020-03-31 20:28:42 +0430

    Register MirzaDbContext in IOC container

--- src/Mirza.Web/Startup.cs
+++ src/Mirza.Web/Startup.cs
@@ -1,15 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Mirza.Web.Data;
 
 namespace Mirza.Web
 {
@@ -26,6 +21,9 @@ namespace Mirza.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddDbContext<MirzaDbContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("MirzaDbContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
