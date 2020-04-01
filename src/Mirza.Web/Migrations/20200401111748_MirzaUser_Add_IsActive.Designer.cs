df0fb2af305beadaf1271284b9f4e03d9b597611
Kia Panahi Rad <kia.panahirad@gmail.com>
2020-04-01 15:48:27 +0430

    Add db migraiton scripts for MirzaUser IsActive field

--- src/Mirza.Web/Migrations/20200401111748_MirzaUser_Add_IsActive.Designer.cs
+++ src/Mirza.Web/Migrations/20200401111748_MirzaUser_Add_IsActive.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Mirza.Web.Data;
 
 namespace Mirza.Web.Migrations
 {
     [DbContext(typeof(MirzaDbContext))]
-    partial class MirzaDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200401111748_MirzaUser_Add_IsActive")]
+    partial class MirzaUser_Add_IsActive
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Mirza.Web/Migrations/20200401111748_MirzaUser_Add_IsActive.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Mirza.Web.Migrations
+{
+    public partial class MirzaUser_Add_IsActive : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Users",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Users");
+        }
+    }
+}
