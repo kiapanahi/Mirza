35f59065bc8846044caf89aecb61d1c50b5fc17b
Kia Panahi Rad <kia.panahirad@gmail.com>
2020-04-03 01:24:14 +0430

    Seed in-memory test database with users and teams

--- tests/Mirza.Web.UnitTests/ServiceTests/UserServiceTests/UserServiceTestBase.cs
+++ tests/Mirza.Web.UnitTests/ServiceTests/UserServiceTests/UserServiceTestBase.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging.Abstractions;
 using Mirza.Web.Data;
+using Mirza.Web.Models;
 using Mirza.Web.Services.User;
 
 namespace Mirza.Web.UnitTests.ServiceTests.UserServiceTests
@@ -17,7 +20,36 @@ namespace Mirza.Web.UnitTests.ServiceTests.UserServiceTests
 
             _inMemoryDbContext = new MirzaDbContext(contextOptions);
 
+            SeedData(_inMemoryDbContext);
+
             UserService = new UserService(_inMemoryDbContext, new NullLogger<UserService>());
+        }
+
+        private void SeedData(MirzaDbContext context)
+        {
+            SeedTeams(context);
+            SeedUsers(context);
+        }
+
+        private void SeedUsers(MirzaDbContext context)
+        {
+            var users = Enumerable.Range(1, 5).Select(idx => new MirzaUser
+            {
+                AccessKeys = new List<AccessKey> { new AccessKey("01234567890123456789012345678901") },
+                Email = $"user{idx}@example.com",
+                IsActive = true,
+                FirstName = $"firstname {idx}",
+                LastName = $"lastname {idx}"
+            });
+            context.UserSet.AddRange(users);
+            context.SaveChanges();
+        }
+
+        private void SeedTeams(MirzaDbContext context)
+        {
+            var teams = Enumerable.Range(1, 5).Select(idx => new Team { Name = $"team-{idx}" });
+            context.TeamSet.AddRange(teams);
+            context.SaveChanges();
         }
 
         protected IUserService UserService { get; private set; }
