f964ca8e6e5b7708badbb30a519e9feb904c73a3
Kia Panahi Rad <kia.panahirad@gmail.com>
2020-04-02 17:32:31 +0430

    Implement basic CRUD operation actions in UsersController

--- src/Mirza.Web/Controllers/UsersController.cs
+++ src/Mirza.Web/Controllers/UsersController.cs
@@ -1,52 +1,101 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using Mirza.Web.Models;
+using Mirza.Web.Services.User;
 
 namespace Mirza.Web.Controllers
 {
-    [Route("api/[controller]")]
     [ApiController]
+    [Route("api/[controller]")]
+    [Produces("application/json", "text/plain", "text/html")]
+    [Consumes("application/json")]
     public class UsersController : ControllerBase
     {
+        private readonly IUserService _userService;
+        private readonly ILogger<UsersController> _logger;
 
-        // GET: api/Users
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<MirzaUser>>> List()
+        public UsersController(IUserService userService, ILogger<UsersController> logger)
         {
-            return await Task.FromResult(StatusCode(501, "Not Implemented")).ConfigureAwait(false);
+            _userService = userService;
+            _logger = logger;
         }
 
-        // GET: api/Users/5
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<MirzaUser>>> List() => await Task.FromResult(StatusCode(501, "Not Implemented")).ConfigureAwait(false);
+
+
         [HttpGet("{id}")]
         public async Task<ActionResult<MirzaUser>> Detail(int id)
         {
-            return await Task.FromResult(StatusCode(501, "Not Implemented")).ConfigureAwait(false);
+            try
+            {
+                var user = await _userService.GetUser(id).ConfigureAwait(false);
+                if (user == null)
+                {
+                    return NotFound();
+                }
+                return Ok(new
+                {
+                    user.Id,
+                    user.FirstName,
+                    user.LastName,
+                    user.Email,
+                    user.IsActive
+                });
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e.ToString());
+                return StatusCode(500, $"Internal Error. LogId: {HttpContext.TraceIdentifier}");
+            }
         }
 
-        // PUT: api/Users/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
-        // more details see https://aka.ms/RazorPagesCRUD.
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutUser(int id, MirzaUser user)
-        {
-            return await Task.FromResult(StatusCode(501, "Not Implemented")).ConfigureAwait(false);
-        }
-
-        // POST: api/Users
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for
         // more details see https://aka.ms/RazorPagesCRUD.
         [HttpPost]
         public async Task<ActionResult<MirzaUser>> PostUser(MirzaUser user)
         {
-            return await Task.FromResult(StatusCode(501, "Not Implemented")).ConfigureAwait(false);
+            try
+            {
+                var registeredUser = await _userService.Register(user).ConfigureAwait(false);
+                return CreatedAtAction(nameof(Detail), new { id = registeredUser.Id }, null);
+            }
+            catch (ArgumentNullException)
+            {
+                return BadRequest("Input was null");
+            }
+            catch (UserModelValidationException e)
+            {
+                return BadRequest(e.ValidationErrors);
+            }
+            catch (DuplicateEmailException e)
+            {
+                return BadRequest(e.Message);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e.ToString());
+                return StatusCode(500, $"Internal Error. LogId: {HttpContext.TraceIdentifier}");
+            }
         }
 
         // DELETE: api/Users/5
         [HttpDelete("{id}")]
         public async Task<ActionResult<MirzaUser>> DeleteUser(int id)
         {
-            return await Task.FromResult(StatusCode(501, "Not Implemented")).ConfigureAwait(false);
+            try
+            {
+                await _userService.DeleteUser(id).ConfigureAwait(false);
+                return NoContent();
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e.ToString());
+                return StatusCode(500, $"Internal Error. LogId: {HttpContext.TraceIdentifier}");
+            }
         }
     }
 }
