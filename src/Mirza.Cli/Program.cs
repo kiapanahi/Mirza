36a1a0e833b3ad0b8855f2f76d69ca47e526e88f
Kia Panahi Rad <kia.panahirad@gmail.com>
2020-04-03 14:07:16 +0430

    Add empty cli console app project

---
+++ src/Mirza.Cli/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mirza.Cli
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
