c5026e2416fd2c95b66cd5afc9b10bc9b273dafc
Kia Panahi Rad <kia.panahirad@gmail.com>
2020-04-09 13:32:17 +0430

    Add constructor overloads for team for convenience

--- src/Mirza.Web/Models/Team.cs
+++ src/Mirza.Web/Models/Team.cs
@@ -8,6 +8,20 @@ namespace Mirza.Web.Models
 {
     public class Team
     {
+        public Team()
+        {
+        }
+
+        public Team(string name)
+        {
+            Name = name;
+        }
+
+        public Team(int id, string name) : this(name)
+        {
+            Id = id;
+        }
+
         public int Id { get; set; }
         public string Name { get; set; }
 
