b7e28e73de5f532806918e1736ec8cddd327f35d
Kia Panahi Rad <kia.panahirad@gmail.com>
2020-04-10 23:07:17 +0430

    Add DTO for access key list page

---
+++ src/Mirza.Web/Dto/AccessKeyListItem.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mirza.Common;
+using Mirza.Web.Models;
+
+namespace Mirza.Web.Dto
+{
+    public class AccessKeyListItem
+    {
+        public int Id { get; set; }
+        public string Key { get; set; }
+        public AccessKeyState State { get; set; }
+        public DateTime Expiration { get; set; }
+        public string ExpirationPersian => Utils.GetPersianDate(Expiration);
+        public bool IsActive => State == AccessKeyState.Active && Expiration > DateTime.UtcNow;
+
+        public AccessKeyListItem(int id, string key, AccessKeyState state, DateTime expirationDate)
+        {
+            Id = id;
+            State = state;
+            Expiration = expirationDate;
+            var sb = new StringBuilder(key);
+
+            Key = sb.Remove(3, 27)
+                .Insert(3, new string('*', 27))
+                .ToString();
+        }
+
+        public AccessKeyListItem(AccessKey key) : this(key.Id, key.Key, key.State, key.Expiration)
+        {
+        }
+    }
+}
---
+++ tests/Mirza.Web.UnitTests/ModelTests/AccessKeyListItemTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore.Migrations.Operations;
+using Mirza.Web.Dto;
+using Mirza.Web.Models;
+using Xunit;
+
+namespace Mirza.Web.UnitTests.ModelTests
+{
+    public class AccessKeyListItemTests
+    {
+        [Fact]
+        public void Converts_Domain_Model_To_Dto()
+        {
+            var accessKey = new AccessKey("01234567890123456789012345678901")
+            {
+                Id = 1,
+                OwnerId = 5,
+                State = AccessKeyState.Active,
+                Expiration = DateTime.Parse("2020-04-10")
+            };
+
+            var dto = new AccessKeyListItem(accessKey);
+
+            Assert.NotNull(dto);
+            Assert.Equal("1399/01/22", dto.ExpirationPersian);
+            Assert.Equal("012***************************01", dto.Key);
+            Assert.Equal(1, dto.Id);
+            Assert.Equal(AccessKeyState.Active, dto.State);
+        }
+    }
+}
