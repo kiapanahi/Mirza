2ea7acb9a2ce37a58bd6789911b8836bffa66640
Kia Panahi Rad <kia.panahirad@gmail.com>
2020-04-17 17:32:18 +0430

    Add Report page

---
+++ src/Mirza.Web/Pages/Report.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Mirza.Web.Dto;
+using Mirza.Web.Services.Report;
+
+namespace Mirza.Web.Pages
+{
+    public class ReportModel : PageModel
+    {
+        private readonly IReportService _reportService;
+
+        public ReportModel(IReportService reportService)
+        {
+            _reportService = reportService;
+        }
+
+        public IEnumerable<WorkLogReportOutput> Report { get; set; }
+
+        public IActionResult OnGetAsync()
+        {
+            Report = _reportService.GetReport();
+
+            return Page();
+        }
+    }
+}
