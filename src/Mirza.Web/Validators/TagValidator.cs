495096be8df943f018da16f8474481bbcd60dc1b
Kia Panahi Rad <kia.panahirad@gmail.com>
2020-04-30 14:25:17 +0430

    Add tag validator

---
+++ src/Mirza.Web/Validators/TagValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using Mirza.Web.Models;
+
+namespace Mirza.Web.Validators
+{
+    public class TagValidator : AbstractValidator<Tag>
+    {
+        public TagValidator()
+        {
+            RuleFor(t => t.Value)
+                .NotEmpty().WithMessage("Tag value cannot be empty")
+                .MaximumLength(256).WithMessage("tag value cannot exceed 256 character");
+        }
+    }
+}
